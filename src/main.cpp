ab09b9fb1c1c8781d3c6cc4e869f049cc041f6c5
Percy <2191295352@qq.com>
2026-06-10 12:47:54 +0800

    test DHT11 sensor reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,33 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include "secrets.h"
+#include <DHT.h>
 
-void connectWiFi() {
-  Serial.print("Connecting to WiFi");
+#define DHT_PIN 4
+#define DHT_TYPE DHT11
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println();
-  Serial.println("WiFi connected");
-  Serial.print("ESP32 IP: ");
-  Serial.println(WiFi.localIP());
-}
-
-void sendSensorData() {
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("WiFi disconnected");
-    return;
-  }
-
-  HTTPClient http;
-  http.begin(SERVER_URL);
-  http.addHeader("Content-Type", "application/json");
-
-  String payload = "{\"device_id\":\"ESP32-ROOM-001\",\"temperature\":39.5,\"humidity\":63.2}";
-
-  int httpCode = http.POST(payload);
-
-  Serial.print("HTTP status: ");
-  Serial.println(httpCode);
-
-  if (httpCode > 0) {
-    String response = http.getString();
-    Serial.print("Response: ");
-    Serial.println(response);
-  }
-
-  http.end();
-}
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
+    Serial.begin(115200);
+    delay(1000);
 
-  Serial.println("ESP32 indoor monitor started");
-  connectWiFi();
+    Serial.println("DHT11 test started");
+    dht.begin();
 }
 
 void loop() {
-  sendSensorData();
-  delay(5000);
+    delay(2000);
+
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+
+    if (isnan(temperature) || isnan(humidity)) {
+        Serial.println("Failed to read DHT11");
+        return;
+    }
+
+    Serial.print("Temperature: ");
+    Serial.print(temperature, 1);
+    Serial.print(" C | Humidity: ");
+    Serial.print(humidity, 1);
+    Serial.println(" %");
 }
